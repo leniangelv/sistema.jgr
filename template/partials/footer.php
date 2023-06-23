4d54cd8f0ad3263561d8cc54b7d84c2c724a3037
Leniangel Pérez <102707929+leniangelv@users.noreply.github.com>
2023-06-23 14:08:03 -0400

    footer.php

--- template/partials/footer.php
+++ template/partials/footer.php
@@ -3,8 +3,8 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h4 class="text-white mb-4">Liceo Juan Germán Roscio</h4>
-					<h6><strong class="text-white">Contacto:</strong></h6>
+				<h4 class="text-white mb-3"><b>Liceo Juan Germán Roscio</b></h4>
+					<h5><strong class="text-white">Contacto:</strong></h5>
 					<ol class="text-white">
 						<li><i class="lead">Teléfono: </i><strong>0246-4312178</strong></li>
 						<li><i class="lead">Correo Electrónico: </i><strong>uejuangerm@gmail.com</strong></li>
@@ -14,12 +14,11 @@
 			</div>
 			<div class="row d-flex" style='justify-content: center;'>
 				<div class="col-12">
-					<img src="<?php echo $root . 'assets/img/logo.png'?>" class='w-25'>
+					<img src="<?php echo $root . 'assets/img/logo.png'?>" class='w-25 p-3'>
 				</div>
 			</div>
 		</div>	
 	</footer>
-
 
   </body>
 </html>
